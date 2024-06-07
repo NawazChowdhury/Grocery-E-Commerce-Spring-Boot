f24e4217e51c564248090832c00f6197d8009eba
Nawaz Chowdhury <Nawaz.Chowdhury@trios.com>
2024-06-07 12:17:32 -0400

    v3

---
+++ src/main/java/com/example/ecommerce_nawaz/EcommerceNawazApplication.java
@@ -0,0 +1,13 @@
+package com.example.ecommerce_nawaz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceNawazApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommerceNawazApplication.class, args);
+	}
+
+}
